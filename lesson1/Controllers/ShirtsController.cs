a0383ebe4f85e86ee7faba8c6dcc4495e1275e13
李子扬 <513523798@qq.com>
2025-11-21 00:08:45 +0800

    API with model
    model

---
+++ lesson1/Controllers/ShirtsController.cs
@@ -0,0 +1,40 @@
+﻿using lesson1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lesson1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ShirtsController: ControllerBase
+    {
+        [HttpGet]
+        public string GetShirts()
+        {
+            return "Here are your shirts!";
+        }
+
+        [HttpGet("{id}")]
+        public string GetShirtById(int id)
+        {
+            return $"Here is shirt with id: {id}";
+        }
+
+        [HttpPost]
+        public string CreateShirt([FromBody]Shirt shirt)
+        {
+            return $"Created a shirt.";
+        }
+
+        [HttpPut("{id}")]
+        public string UpdateShirt(int id)
+        {
+            return $"Updated shirt with id: {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string DeleteShirt(int id)
+        {
+            return $"Deleted shirt with id: {id}";
+        }
+    }
+}
---
+++ lesson1/Models/Shirt.cs
@@ -0,0 +1,19 @@
+﻿using lesson1.Validations;
+using System.ComponentModel.DataAnnotations;
+
+namespace lesson1.Models
+{
+    public class Shirt
+    {
+        public int ShirtId { get; set; }
+        public string? Brand { get; set; }
+        public string? Color { get; set; }
+
+        [Shirt_EnsureCorrectSizing]
+        public int Size { get; set; }
+        public string? Gender { get; set; }
+        public double? price { get; set; }
+
+
+    }
+}
